a6ba4a4b09d6350d09c2a06adfbb87a9944bf81d
sustersic <sustersicjan@gmail.com>
2024-10-09 01:20:16 +0300

    Changed spins, added easter egg, fixed text.

--- visualize.js
+++ visualize.js
@@ -2,6 +2,7 @@ import BubbleSortVisualizer from "./bubblesort.js";
 import InsertionSortVisualizer from "./insertionsort.js";
 
 let activeVisualizers = [];
+let chaoticVisualizers = [];
 
 createDemo();
 
@@ -44,4 +45,49 @@ function createDemo() {
   }
 }
 
+function chaos() {
+  resetChaos();
+  visualizerChaos.style.display = "flex";
+  for (let i = 1; i <= 40; i++) {
+    if (Math.random() < 0.5) {
+      const container = document.createElement("div");
+      container.classList.add("visualizer");
+      const sv = new BubbleSortVisualizer(20, container, true);
+      activeVisualizers.push(sv);
+
+      const nameDiv = document.createElement("div");
+      nameDiv.classList.add("visualizerName");
+      nameDiv.textContent = "Bubble Sort";
+      container.appendChild(nameDiv);
+
+      visualizerChaos.appendChild(container);
+    } else {
+      const container = document.createElement("div");
+      container.classList.add("visualizer");
+      const sv = new InsertionSortVisualizer(20, container, true);
+      activeVisualizers.push(sv);
+
+      const nameDiv = document.createElement("div");
+      nameDiv.classList.add("visualizerName");
+      nameDiv.textContent = "Insertion Sort";
+      container.appendChild(nameDiv);
+
+      visualizerChaos.appendChild(container);
+    }
+  }
+}
+
+function resetChaos() {
+  chaoticVisualizers.forEach((vis) => vis.stopAnimation());
+  chaoticVisualizers = [];
+  visualizerChaos.innerHTML = "";
+  visualizerChaos.style.display = "none";
+}
+
 document.getElementById("restartButton").addEventListener("click", createDemo);
+document
+  .getElementById("visualizersContainer")
+  .addEventListener("click", chaos);
+document
+  .getElementById("visualizerChaos")
+  .addEventListener("click", resetChaos);
